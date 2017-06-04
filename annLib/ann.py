110f94bd036788166d9da0c13e2a0ab0ae512a97
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-06-03 23:00:10 -0700

    wip

--- annLib/ann.py
+++ annLib/ann.py
@@ -1,1 +1,6 @@
-
+# Class to execute the artificial neural network
+class ANN:
+    # Nested class for individual neurons
+    class Neuron:
+        def __init__(self, weights):
+            self.weights = weights[:]
