090741dc3cda18e605c68f1a0b646c434ea63c67
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-06-03 23:10:09 -0700

    wip

--- annLib/ann.py
+++ annLib/ann.py
@@ -4,3 +4,22 @@ class ANN:
     class Neuron:
         def __init__(self, weights):
             self.weights = weights[:]
+
+    # Constructor simply sets meta member variables
+    def __init__(self, train_dir, structure_file, alpha, iters):
+        self.train_dir = train_dir
+        self.structure_file = structure_file
+        self.alpha = alpha
+        self.iters = iters
+
+        # Set by later functions
+        self.num_chars = 0
+
+    def get_num_chars(self):
+        found_chars = []
+        for filename in os.listdir(train_dir):
+            this_char = filename[0]
+            if this_char not in found_chars:
+                found_chars.append(this_char)
+
+        self.num_chars = len(found_chars)
