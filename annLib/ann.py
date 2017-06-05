6fdd0c2f7340485a19500d89996d10e3bb9c78f5
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-06-04 17:00:10 -0700

    commenting

--- annLib/ann.py
+++ annLib/ann.py
@@ -2,19 +2,19 @@ import os
 import math
 import random
 
-# Class to execute the artificial neural network
 class ANN:
-    # Nested class for individual neurons
+    """Class to execute the artificial neural network"""
     class Neuron:
+        """Nested class for individual neurons"""
         def __init__(self):
             self.weights = []
 
-        # Assign the Neuron its weights
         def assign_weights(self, weights_line):
+            """Assign the Neuron its weights"""
             self.weights = [float(w) for w in weights_line.strip(' ').split(' ')]
 
-    # Constructor simply sets meta member variables
     def __init__(self, train_dir, structure_file, weights_file, alpha, iters):
+        """Constructor simply sets meta member variables"""
         self.train_dir = train_dir
         self.structure_file = structure_file
         self.weights_file = weights_file
@@ -25,8 +25,8 @@ class ANN:
         self.num_chars = 0
         self.layers = []
 
-    # Get the number of characters we are training on
     def get_num_chars(self):
+        """Get the number of characters we are training on"""
         found_chars = []
         for filename in os.listdir(self.train_dir):
             this_char = filename[0]
@@ -35,15 +35,15 @@ class ANN:
 
         self.num_chars = len(found_chars)
 
-    # Construct the Neuron web based on the structure file
     def build_structure(self):
+        """Construct the Neuron web based on the structure file"""
         with open(self.structure_file, 'r') as file:
             layer_depths = ['1'] + file.read().split('\n')[:-1]
             self.layers = [[ANN.Neuron() for n in range(int(l))] for l in layer_depths]
             file.close()
 
-    # Assign all the Neurons their starting (or testing) weights
     def assign_all_weights(self, dummy=0.01, mini=-10, maxi=10):
+        """Assign all the Neurons their starting (or testing) weights"""
         # Set all dummy neuron weights and use 2-d list to store weights
         for l in range(len(self.layers[1:])):
             this_layer_weights = []
@@ -75,3 +75,7 @@ class ANN:
                 n = 0
             else:
                 self.layers[l][n].assign_weights(line)
+
+    def save_network(self):
+        """Save the structure and learned weights for this network"""
+        pass
