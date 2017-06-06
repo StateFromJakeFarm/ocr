23b19f009a6a6d24536b372ed120809eb3756c88
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-06-05 23:16:48 -0700

    re-add files and create ANN.build() method

---
+++ ann.py
@@ -0,0 +1,82 @@
+import os
+import math
+import random
+
+class ANN:
+    """Class to execute the artificial neural network"""
+    class Neuron:
+        """Nested class for individual neurons"""
+        def __init__(self):
+            self.weights = []
+
+        def assign_weights(self, weights_line):
+            """Assign the Neuron its weights"""
+            self.weights = [float(w) for w in weights_line.strip(' ').split(' ')]
+
+    def __init__(self, train_dir, structure_file, alpha, iters):
+        """Constructor simply sets meta member variables"""
+        self.train_dir = train_dir
+        self.structure_file = structure_file
+        self.alpha = alpha
+        self.iters = iters
+
+        # Members set internally
+        self.num_chars = 0
+        self.layers = []
+
+    def get_num_chars(self):
+        """Get the number of characters we are training on"""
+        found_chars = []
+        for filename in os.listdir(self.train_dir):
+            this_char = filename[0]
+            if this_char not in found_chars:
+                found_chars.append(this_char)
+
+        self.num_chars = len(found_chars)
+
+    def build_structure(self, file):
+        """Construct the Neuron web based on the structure file"""
+        layer_depths = ['1'] + file.readline().strip('\n').split(' ')
+        self.layers = [[ANN.Neuron() for n in range(int(l))] for l in layer_depths]
+
+    def assign_all_weights(self, file, dummy=0.01, mini=-0.5, maxi=0.5):
+        """Assign all the Neurons their starting (or testing) weights"""
+        # Set all dummy neuron weights and use 2-d list to store weights
+        for l in range(len(self.layers[1:])):
+            this_layer_weights = []
+            for n in range(len(self.layers[l+1])):
+                this_layer_weights.append(dummy)
+            self.layers[0][0].weights.append(this_layer_weights)
+
+        # Attempt to grab weights from file
+        all_lines = file.read().split('\n')[:-1]
+
+        # Randomize starting weights if file does not specify weights
+        if len(all_lines) > 0:
+            for l in range(len(self.layers[1:-1])):
+                for n in self.layers[l+1]:
+                    weights_ln = ''
+                    for w in self.layers[l+2]:
+                        weights_ln += str( random.uniform(mini, maxi) ) + ' '
+                    all_lines.append(weights_ln)
+
+        # Assign weights to each neuron
+        l = 1
+        n = 0
+        for line in all_lines:
+            if line == '' or line == '\0':
+                l += 1
+                n = 0
+            else:
+                self.layers[l][n].assign_weights(line)
+
+    def build(self):
+        """Construct the network and load weights (if any)"""
+        with open(self.structure_file, 'r') as file:
+            self.build_structure(file)
+            self.assign_all_weights(file)
+            file.close()
+
+    def save_network(self):
+        """Save the structure and learned weights for this network"""
+        pass
---
+++ imgFuncs.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+
+from PIL import Image
+from os import listdir
+import os
+import sys
+import subprocess
+import shutil
+import glob
+
+def make_char_square(path, newPath, sideLen, highLo=None):
+    """Crop image to character and save as square bitmap"""
+    img = None
+    if type(path) is str:
+        img = Image.open(path).convert('L')
+    else:
+        img = path
+
+    imgX, imgY = img.size
+
+    pixels = img.load()
+
+    # high-pass/low-pass filter
+    if highLo:
+        for y in range(imgY):
+            for x in range(imgX):
+                if pixels[x,y] <= highLo:
+                    pixels[x,y] = 0
+                else:
+                    pixels[x,y] = 255
+
+    bounds = [-1,-1,-1,-1]
+    # start x, end x
+    for x in range(imgX):
+        sawChar = False
+        for y in range(imgY):
+            if bounds[0] == -1:
+                if pixels[x,y] == 0:
+                    bounds[0] = x
+                    sawChar = True
+            else:
+                if pixels[x,y] == 0:
+                    if x != imgX-1:
+                        sawChar = True
+                    else:
+                        bounds[2] = x-1
+                elif y == imgY-1 and not sawChar:
+                    bounds[2] = x-1
+
+        if bounds[0] != -1 and not sawChar:
+            break
+
+    # start y, end y
+    for y in range(imgY):
+        sawChar = False
+        for x in range(imgX):
+            if bounds[1] == -1:
+                if pixels[x,y] == 0:
+                    bounds[1] = y
+                    sawChar = True
+            else:
+                if pixels[x,y] == 0:
+                    if y != imgY-1:
+                        sawChar = True
+                    else:
+                        bounds[3] = imgY-1
+                elif x == imgX-1 and not sawChar:
+                    bounds[3] = y-1
+
+        if bounds[1] != -1 and not sawChar:
+            break
+
+    # bad strip
+    if -1 in bounds:
+        return
+
+    img = img.crop(tuple(bounds)).resize((sideLen,sideLen), Image.LANCZOS)
+    img.convert('RGB').save(newPath)
+
+def create_formatted(rawFolder, bmpFolder, sideLen):
+    """Convert images of characters to properly-formatted images"""
+    clear_folder(bmpFolder)
+
+    for char in listdir(rawFolder):
+        i = 0
+        for f in listdir(os.path.join(rawFolder, char)):
+            rawPath = os.path.join(rawFolder, char, f)
+            bmpPath = os.path.join(bmpFolder, char+str(i)+'.bmp')
+            print(rawPath + ' --(' + str(sideLen) + 'x' + str(sideLen) + ')-> ' + bmpPath)
+            make_char_square(rawPath, bmpPath, sideLen)
+            i += 1
+
+def fullImgBmp(rawPath, bmpFolder, sideLen, highLo):
+    clear_folder(bmpFolder)
+
+    img = Image.open(rawPath).convert('L')
+    imgX, imgY = img.size
+    pixels = img.load()
+
+    for y in range(imgY):
+        for x in range(imgX):
+            if pixels[x,y] <= highLo:
+                pixels[x,y] = 0
+            else:
+                pixels[x,y] = 255
+
+    bounds = [-1,0,imgX,imgY]
+    i = 0
+    for x in range(imgX):
+        sawChar = False
+        for y in range(imgY):
+            if bounds[0] == -1:
+                if pixels[x,y] == 0:
+                    bounds[0] = x
+                    sawChar = True
+            else:
+                if pixels[x,y] == 0:
+                    sawChar = True
+                elif y == imgY-1 and not sawChar:
+                    bounds[2] = x-1
+
+        if bounds[0] != -1 and not sawChar:
+            i += 1
+            make_char_square(img.crop(tuple(bounds)), os.path.join(bmpFolder, str(i) + '.bmp'), sideLen)
+            bounds = [-1,0,imgX,imgY]
+
+def clear_folder(folderPath):
+    if os.path.isdir(folderPath):
+        shutil.rmtree(folderPath)
+    os.makedirs(folderPath)
+
+def run_ann(argsList, giveString=False):
+    ann = subprocess.Popen(argsList, stdout=subprocess.PIPE)
+    outStr = ''
+    curStr = ''
+    while True:
+        out = ann.stdout.read(1)
+        if out == '' and ann.poll() != None:
+            break
+        if out != '':
+            if giveString:
+                if str(out) == '\n' or str(out) == '\0':
+                    if len(curStr) == 1:
+                        outStr += str(curStr)
+                    curStr = ''
+                else:
+                    curStr += str(out)
+            else:
+                sys.stdout.write(out)
+                sys.stdout.flush()
+
+    return outStr
